f3c9f7ce60774b9dad45ab0544859060b4286c40
shokostudy <shokostudy@gmail.com>
2017-09-17 03:48:47 +0900

    円グラフjqplotに変更 ・凡例は名前＋値にしたい ・タイトルをタップして年月変更可にしたい ・localstorageの値を反映させる 折れ線グラフデータは毎回新規localstorage作成の状態

--- www/js/pieGraph.js
+++ www/js/pieGraph.js
@@ -1,81 +1,81 @@
-// 口座残高割合(pie)
-function getRatioData(){
-    // 金額をランダムに返す（単位：万）
-    var randomScalingFactor = function() {
-        return Math.round(Math.random() * 1000);
-    };
+// // 口座残高割合(pie)（カスタム後）
+// function getRatioData(){
+//     // 金額をランダムに返す（単位：万）
+//     var randomScalingFactor = function() {
+//         return Math.round(Math.random() * 1000);
+//     };
+// 
+//     var date = new Date();
+//     var year = date.getFullYear();
+//     var month = date.getMonth();
+//     
+//     // パーセント表示用にデータを整形
+//     var dataArray = [];
+//     var dataSum = 0;
+//     for(var i = 0; i < 4; i++){
+//         var dataTmp = randomScalingFactor();
+//         dataSum += dataTmp;
+//         dataArray[i] = dataTmp;
+//     }
+//     for(var i = 0; i < 5; i++){
+//         dataArray[i] = Math.round((dataArray[i] / dataSum) * 100);
+//     }
+//     
+//     // タイトルを作成
+//     var title = year + '/' + (month + 1) + '　　total：' + (dataSum * 1000) + '';
+//     
+//     
+//     var chartColors = window.chartColors;
+//     var color = Chart.helpers.color;
+//     
+//     // グラフ用データを作成
+//     var configData = {
+//         type: 'pie',
+//         data: {
+//             datasets: [{
+//                 data: dataArray,
+//                 backgroundColor: [
+//                     color(chartColors.orange).alpha(1.0).rgbString(),
+//                     color(chartColors.yellow).alpha(1.0).rgbString(),
+//                     color(chartColors.green).alpha(1.0).rgbString(),
+//                     color(chartColors.blue).alpha(1.0).rgbString(),
+//                 ],
+//                 label: 'Dataset 1'
+//             }],
+//             labels: [
+//                 "A-Bank",
+//                 "B-Bank",
+//                 "C-Bank",
+//                 "D-Bank"
+//             ]
+//         },
+//         options: {
+//             responsive: true,
+//             title: {
+//                 display: true,
+//                 text: title,
+//                 fontSize:14
+//             },
+//             tooltips: {
+//               callbacks: {
+//                 label: function (tooltipItem, data) {
+//                   return data.labels[tooltipItem.index]
+//                     + ": "
+//                     + data.datasets[0].data[tooltipItem.index]
+//                     + " %";
+//                 }
+//               }
+//             },
+//             legend: {
+//               display: false
+//             }
+//         }
+//     };
+//     
+//     return configData;
+// }
 
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth();
-    
-    // パーセント表示用にデータを整形
-    var dataArray = [];
-    var dataSum = 0;
-    for(var i = 0; i < 4; i++){
-        var dataTmp = randomScalingFactor();
-        dataSum += dataTmp;
-        dataArray[i] = dataTmp;
-    }
-    for(var i = 0; i < 5; i++){
-        dataArray[i] = Math.round((dataArray[i] / dataSum) * 100);
-    }
-    
-    // タイトルを作成
-    var title = year + '/' + (month + 1) + '　　total：' + (dataSum * 1000) + '';
-    
-    
-    var chartColors = window.chartColors;
-    var color = Chart.helpers.color;
-    
-    // グラフ用データを作成
-    var configData = {
-        type: 'pie',
-        data: {
-            datasets: [{
-                data: dataArray,
-                backgroundColor: [
-                    color(chartColors.orange).alpha(1.0).rgbString(),
-                    color(chartColors.yellow).alpha(1.0).rgbString(),
-                    color(chartColors.green).alpha(1.0).rgbString(),
-                    color(chartColors.blue).alpha(1.0).rgbString(),
-                ],
-                label: 'Dataset 1'
-            }],
-            labels: [
-                "A-Bank",
-                "B-Bank",
-                "C-Bank",
-                "D-Bank"
-            ]
-        },
-        options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: title,
-                fontSize:14
-            },
-            tooltips: {
-              callbacks: {
-                label: function (tooltipItem, data) {
-                  return data.labels[tooltipItem.index]
-                    + ": "
-                    + data.datasets[0].data[tooltipItem.index]
-                    + " %";
-                }
-              }
-            },
-            legend: {
-              display: false
-            }
-        }
-    };
-    
-    return configData;
-}
-
-// // 口座残高割合(polar area chart)
+// // 口座残高割合(polar area chart)（デフォルト）
 // function getRatioData(){
 //     // 金額をランダムに返す（単位：万）
 //     var randomScalingFactor = function() {
@@ -161,31 +161,47 @@ function getRatioData(){
 //     return configData;
 // }
 
-$(document).ready(function(){
-  plot2 = jQuery.jqplot('chart2', 
-    [[['A-bank', 500],['B-bank', 300], ['C-bank', 700], 
-    ['D-bank', 250]]], 
-    {
-    　seriesColors:['#ff9f40','#ffcd56','#4bc0c0','#36a2eb'],
-      title: '', 
-      seriesDefaults: {
-        shadow: false, 
-        renderer: jQuery.jqplot.PieRenderer, 
-        rendererOptions: { 
-          startAngle: 180, 
-          sliceMargin: 4, 
-          showDataLabels: true } 
-      }, 
-      legend: { show:true, location: 'e' },
-       // グラフ全体を囲むグリッドの設定    														
-        grid: {														
-            // グラフを囲む枠線の太さ、0で消える														
-            borderWidth: 0,														
-            // 背景色を透明に														
-            background: 'transparent',														
-            // 影もいらない														
-            shadow: false,														
+// jqplot使用円グラフ作成
+    $(document).ready(function(){
+        // グラフ作成
+        plot2 = jQuery.jqplot('chart2', 
+        [[['A-bank', 500],['B-bank', 300], ['C-bank', 700], 
+        ['D-bank', 250]]], 
+        {
+            seriesColors:['#ff9f40','#ffcd56','#4bc0c0','#36a2eb'],
+            title: '', 
+            seriesDefaults: {
+                shadow: false, 
+                renderer: jQuery.jqplot.PieRenderer, 
+                rendererOptions: { 
+                startAngle: 180, 
+                sliceMargin: 4, 
+                showDataLabels: true } 
+            }, 
+            legend: { show:true, location: 'se' },
+            // グラフ全体を囲むグリッドの設定    														
+            grid: {														
+                // グラフを囲む枠線の太さ、0で消える														
+                borderWidth: 0,														
+                // 背景色を透明に														
+                background: 'transparent',														
+                // 影もいらない														
+                shadow: false,														
+            }
         }
-    }
-  );
-});
+        );
+      
+        // グラフタイトル作成
+        var now_pie = new Date();
+        // 現在の年
+        var year_pie = now_pie.getFullYear();
+        // 現在の月// 
+        var month_pie = now_pie.getMonth() + 1;
+        // 円グラフタイトルを設定
+        document.getElementById("chart2_title").innerHTML = year_pie + '/' + month_pie + '　total : 17500000';
+
+    });
+    
+
+
+
